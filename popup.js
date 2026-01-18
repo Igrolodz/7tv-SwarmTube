8bb84baf3168319876ff2ecfabe19a1abd118599
Igrolodz <116462723+Igrolodz@users.noreply.github.com>
2026-01-18 18:03:05 +0100

    Extension toggle added

--- popup.js
+++ popup.js
@@ -1,3 +1,29 @@
-document.getElementById("btn").addEventListener("click", () => {
-    alert("Extension is working!");
+const toggleBtn = document.getElementById("toggle");
+
+// Initialize toggle state
+chrome.storage.local.get("enabled").then(({ enabled }) => {
+    if (enabled) {
+        toggleBtn.classList.remove("off");
+        toggleBtn.classList.add("on");
+    } else {
+        toggleBtn.classList.remove("on");
+        toggleBtn.classList.add("off");
+    }
+});
+
+var state = true;
+toggleBtn.addEventListener("click", async () => {
+    const { enabled } = await chrome.storage.local.get("enabled");
+    const newState = !(enabled ?? false);
+    
+    await chrome.storage.local.set({enabled: newState});
+
+    // Toggle classes
+    if (newState) {
+        toggleBtn.classList.remove("off");
+        toggleBtn.classList.add("on");
+    } else {
+        toggleBtn.classList.remove("on");
+        toggleBtn.classList.add("off");
+    }
 });
